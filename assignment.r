80af9bd4634fb9739bf204adecdb235664b9331c
Jesus Moreno <moreno.jesus.j@gmail.com>
2016-02-18 15:08:03 -0800

    Jesus Movies

--- assignment.r
+++ assignment.r
@@ -1,1 +1,5 @@
+
 zeb_movies <- list('12 Angry Men', 'Life of Pi', 'The Dark Knight', 'Spirited Away', 'The Great Gatsby')
+
+Jesus_Movies <- list("The Dark Knight", "Mad Max: Fury Road", "Interstellar", "Ex Machina", "Hangover")
+
